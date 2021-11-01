8fe591c855201e6415910b708b2b26118510b31f
Andres Liu <andres.liu22@gmail.com>
2021-11-01 08:43:59 -0700

    Add handlebars

--- public/js/login.js
+++ public/js/login.js
@@ -22,14 +22,15 @@ const loginFormHandler = async (event) => {
   const signupFormHandler = async (event) => {
     event.preventDefault();
   
-    const name = document.querySelector('#name-signup').value.trim();
+    const firstName = document.querySelector('#first_name-signup').value.trim();
+    const lastName = document.querySelector('#last_name-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
   
-    if (name && email && password) {
+    if (firstName && lastName && email && password) {
       const response = await fetch('/api/users', {
         method: 'POST',
-        body: JSON.stringify({ name, email, password }),
+        body: JSON.stringify({ firstName, lastName, email, password }),
         headers: { 'Content-Type': 'application/json' },
       });
   
